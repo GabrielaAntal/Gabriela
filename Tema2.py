5a798b1eca70792484165acadaa124586379bab5
Gabriela <gabrielaantall@yahoo.com>
2024-10-22 16:37:13 +0300

    print("gabriela")

---
+++ Tema2.py
@@ -0,0 +1,19 @@
+sir_articol = """Rihanna, la un pas să intre în lumea fotbalului! Cântăreața, așteptată să se implice la un club din Anglia."""
+jumatate=len(sir_articol)//2
+prima_parte=sir_articol[:jumatate]
+a_doua_parte=sir_articol[jumatate:]
+prima_parte=prima_parte.upper()
+prima_parte=prima_parte.strip()
+a_doua_parte=a_doua_parte[::-1]
+a_doua_parte=a_doua_parte.capitalize()
+semnele=(".",",","!")
+a_doua_parte=a_doua_parte.replace[semnele]
+rezultat=prima_parte+a_doua_parte
+
+
+
+
+
+
+
+
